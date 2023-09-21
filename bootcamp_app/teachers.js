e40931885eece316cf1479995b44ed48f536ddb1
Justin Hao <justinhao@live.com>
2023-09-20 20:24:54 -0400

    Parameterize queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = assistance_requests.student_id
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach((row) => {
-    console.log(`${row.cohort}: ${row.teacher}`)
-  })
-});
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+  `;
+
+const cohortName = process.argv[2];
+const value = [cohortName];
+
+pool.query(queryString, value)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`)
+    })
+  });
